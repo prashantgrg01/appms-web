c96454c4f8276f5ff243ce18ef568e21cbc38eee
prashantgrg01 <zxcspgrg.05@live.com>
2017-08-01 22:59:03 +0545

    removed unnecessary files

--- appms/settings.py
+++ appms/settings.py
@@ -119,8 +119,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
